6471927eb4dbaf316097639ff7da7d9d73fef0f1
juanejd <juanes.pio51@gmail.com>
2025-07-27 00:10:13 -0500

    feat: update project settings and create user and movies model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class MovieFavorites(models.Model):
+    id_movie = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    saved_at = models.DateTimeField(auto_now_add=True)
