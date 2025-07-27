7a15f5d147738dfef9b18e1b38b0f7c5b8c26c56
juanejd <juanes.pio51@gmail.com>
2025-07-27 00:56:55 -0500

    fix: change models structure

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=15, unique=True)
-    password = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class User(AbstractUser):
+    age = models.IntegerField()
--- backend/movies/models.py
+++ backend/movies/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from accounts.models import User
 
 
-class MovieFavorites(models.Model):
-    id_movie = models.CharField(max_length=255, unique=True)
+class MovieFavorite(models.Model):
+    id_movie = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
