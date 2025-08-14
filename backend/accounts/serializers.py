bb7e43ee58c8b019ab9c9034e7ae5efe21839d34
juanejd <juanes.pio51@gmail.com>
2025-08-14 00:38:13 -0500

    refactor: add perfomance and validators in the fields username, password and email at serializers

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import User
+from rest_framework_simplejwt.tokens import Token
+from .models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,14 +12,61 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email"]
 
 
+class TokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["full_name"] = user.profile.full_name
+        token["username"] = user.username
+        token["email"] = user.mail
+        token["image"] = user.profile.image
+        token["verified"] = user.profile.verified
+
+        return token
+
+
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ["email", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ["email", "username", "password", "password2"]
+
+    # custom username validation
+    def validate_username(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                {"message": "El nombre de usuario debe tener al menos 5 caracteres"}
+            )
+
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                {"message": "El nombre de usuario ya esta en uso"}
+            )
+        return value
+
+    # custom email validation
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError({"message": "El email ya esta en uso"})
+        return value
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Las contraseñas no coinciden"}
+            )
+        return attrs
 
     def create(self, validated_data):
-        if User.objects.filter(email=validated_data["email"]).exists():
-            raise serializers.ValidationError({"message": "El email ya esta en uso"})
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
         return user
